2adb1e1d1ca7106cbb316749e52069b1b9bd8f2f
Reddeppa kollu <neerajchowdary889@gmail.com>
2023-07-02 02:13:42 +0530

    Login Functionality added

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,7 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use serde_json::json;
 mod CreateAccount;
+mod clientLogin;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -25,8 +26,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 async fn create_account() -> Result<(), Box<dyn Error>> {
     let (username, password, total_hash) = CreateAccount::CreateAccount();
 
-    let mut stream = TcpStream::connect("127.0.0.1:5000").await?;
-    // let mut stream = TcpStream::connect("0.tcp.in.ngrok.io:18096").await?;
+    // let mut stream = TcpStream::connect("127.0.0.1:5000").await?;
+    let mut stream = TcpStream::connect("0.tcp.in.ngrok.io:18096").await?;
     println!("Connected to server");
 
     let json_data = json!({
@@ -47,8 +48,26 @@ async fn create_account() -> Result<(), Box<dyn Error>> {
 }
 
 async fn log_in() -> Result<(), Box<dyn Error>> {
-    // TODO: Implement login logic
-    println!("Log in function not implemented yet");
+    let (username, password, total_hash) = clientLogin::Login();
+
+    let mut stream = TcpStream::connect("127.0.0.1:5050").await?;
+    // let mut stream = TcpStream::connect("0.tcp.in.ngrok.io:18096").await?;
+    println!("Connected to server");
+
+    let json_data = json!({
+        "name": username,
+        "password": password,
+        "TotalHash": total_hash
+    });
+
+    let data = serde_json::to_string(&json_data)?;
+    stream.write_all(data.as_bytes()).await?;
+
+    let mut buffer = [0; 1024];
+    let size = stream.read(&mut buffer).await?;
+    let response_data = String::from_utf8_lossy(&buffer[..size]);
+    println!("Received response: {}", response_data);
+
     Ok(())
 }
 
---
+++ src/clientLogin.rs
@@ -0,0 +1,68 @@
+use std::io;
+extern crate colored;
+use colored::Colorize;
+use sha1::{Digest, Sha1};
+
+
+pub fn Login() -> (String, String, String){
+    println!("{}","++  Login Portal ++".bold().green().italic());
+
+    println!("Enter Username: ");
+    let mut username = String::new();
+    io::stdin().read_line(&mut username).expect("Wrong input");
+    let mut input = healthy_input(&username);
+
+    if input == false{
+        return Login()
+    }
+
+    println!("Enter Password: ");
+    let mut password = String::new();
+    io::stdin().read_line(&mut password).expect("Wrong input");
+    input = healthy_input(&password);
+
+    if input == false{
+        return Login()
+    }
+
+    let(Username,Password, TotalHash) = convert_to_sha(username.trim().to_string(), password.trim().to_string());
+    println!("{}\n{}\n{}",Username,Password,TotalHash);  // This is the final output
+    return(Username,Password,TotalHash)
+  
+}
+
+fn convert_to_sha(username: String, password: String) -> (String, String, String){
+
+    // println!("{},{}", username, password);
+
+    let mut hasher = Sha1::new();
+    let mut hasher_password = Sha1::new();
+    let mut TotalHash = Sha1::new();
+
+    hasher.update(&username);
+    let result = hasher.finalize();
+    let result_str = format!("{:x}", result);
+
+    hasher_password.update(&password);
+    let result_password = hasher_password.finalize();
+    let result_password_str = format!("{:x}", result_password);
+
+    TotalHash.update(&(result_str.to_string() + &result_password_str));
+    let TotalHash = TotalHash.finalize();
+    let TotalHash = format!("{:x}", TotalHash);
+    // println!("{}\n{}",result_str, result_password_str);
+
+    // println!("{}","\n++ Account Created ++".bold().green().italic());
+    return(result_str, result_password_str, TotalHash)
+}
+
+
+fn healthy_input(input: &String) -> bool{
+    if input.trim().is_empty(){
+        println!("{}", "Wrong input... TryAgain".red().bold());
+        false
+    }
+    else{
+        true
+    }
+}
